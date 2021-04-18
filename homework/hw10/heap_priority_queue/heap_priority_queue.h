79dde265f43d995c778ce7c28249b82b3cc4487d
p214smith <p214smith@gmail.com>
2021-04-18 16:19:54 -0400

    homework 10 push

--- homework/hw10/heap_priority_queue/heap_priority_queue.h
+++ homework/hw10/heap_priority_queue/heap_priority_queue.h
@@ -31,26 +31,105 @@ template <typename T>
 bool HeapPriorityQueue<T>::isEmpty()
 {
     //todo
-    return false;
+    return lst.isEmpty();
 }
 
 template <typename T>
 void HeapPriorityQueue<T>::add(const T& item)
 {
-    //todo
-}
+    if (lst.isEmpty())
+        lst.insert(0,item);
+    else
+    {
+    int i = lst.getLength();
+    lst.insert(i,item);
+    int parent =(i-1)/2;
+    int current = i;
+    while (i>0)
+    {
+    if (lst.getEntry(parent)<lst.getEntry(current))
+    {   
+        T swapper = lst.getEntry(parent);
+        lst.setEntry(parent, lst.getEntry(current));
+        lst.setEntry(current,swapper);
+        i = parent;
+        current = i;
+        parent = (i-1)/2;
+    }
+    else 
+    i = 0;
+    }
+    }
+    
+        }
 
 template <typename T>
 void HeapPriorityQueue<T>::remove()
 {
-    //todo
+    if (lst.getLength()==1)
+    lst.remove(0);
+    else 
+    {
+    int i = lst.getLength()-1;
+    lst.setEntry(0,lst.getEntry(i)); 
+    lst.remove(i);
+    i = 0;
+    int leftChild, rightChild;
+    while (i <= lst.getLength()-1)
+    {
+        leftChild = (2 * i )+1;
+        rightChild = (2 * i) +2;
+        if ( i == lst.getLength()-1)
+        break;
+        else if (leftChild > lst.getLength()-1)
+        break;
+        else if (rightChild > lst.getLength() -1)
+    {
+        if (lst.getEntry(i)<lst.getEntry(leftChild))
+        {
+            T swapper = lst.getEntry(i);
+            lst.setEntry(i,lst.getEntry(leftChild));
+            lst.setEntry(leftChild,swapper);
+            i = leftChild+1;
+        }
+        else 
+        i = lst.getLength(); 
+    }
+    else
+        if (lst.getEntry(leftChild)<lst.getEntry(rightChild))
+        {
+            if (lst.getEntry(i)<lst.getEntry(rightChild))
+            {
+                T swapper = lst.getEntry(i);
+                lst.setEntry(i,lst.getEntry(rightChild));
+                lst.setEntry(rightChild,swapper);
+                i = rightChild;
+            }
+            else 
+            i = lst.getLength();
+        }
+        else 
+        {
+            if (lst.getEntry(i)<lst.getEntry(leftChild))
+            {
+                T swapper = lst.getEntry(i);
+                lst.setEntry(i,lst.getEntry(leftChild));
+                lst.setEntry(leftChild,swapper);
+                i = leftChild;
+            }
+            else 
+            i = lst.getLength(); 
+        }
+    }
+    }
+    
 }
 
 template <typename T>
 T HeapPriorityQueue<T>::peek()
 {
     //todo
-    return T();
+    return lst.getEntry(0);
 }
 
 
--- homework/hw10/heap_priority_queue/test_priority_queue.cpp
+++ homework/hw10/heap_priority_queue/test_priority_queue.cpp
@@ -1,27 +1,80 @@
-#include <cassert>
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_COLOUR_NONE
+#include "catch.hpp"
 
-#include "dynamic_array_list.h"
 #include "heap_priority_queue.h"
 
-int main()
-{
+TEST_CASE("Test Construction", "[construction]") {
   HeapPriorityQueue<int>  pq;
-  assert(pq.isEmpty());
-  
+  REQUIRE(pq.isEmpty());
+}
+
+TEST_CASE("Test add", "[add]") {
+  HeapPriorityQueue<int>  pq;
   pq.add(25);
-  assert(!pq.isEmpty());
-  assert(pq.peek() == 25);
+
+  REQUIRE(!pq.isEmpty());
+  REQUIRE(pq.peek() == 25);
 
   pq.add(1);
-  assert(pq.peek() == 25);
+  REQUIRE(pq.peek() == 25);
 
   pq.add(78);
-  assert(pq.peek() == 78);
+  REQUIRE(pq.peek() == 78);
+
+  pq.add(36);
+  REQUIRE(pq.peek() == 78);
+
+  pq.add(48);
+  REQUIRE(pq.peek() == 78);
+
+  pq.add(96);
+  REQUIRE(pq.peek() == 96);
+
+  pq.add(94);
+  REQUIRE(pq.peek() == 96);
+
+  pq.add(108);
+  REQUIRE(pq.peek() == 108);
+
+  pq.add(5);
+  REQUIRE(pq.peek() == 108);
+}
+
+TEST_CASE("Test remove", "[remove]") {
+  HeapPriorityQueue<int>  pq;
+  pq.add(25);
+  pq.add(1);
+  pq.add(78);
+  pq.add(101);
+  pq.add(24);
+  pq.add(27);
+  pq.add(2);
+  pq.add(100);
+  pq.add(108);
+  pq.add(115);
+  pq.add(10);
 
   pq.remove();
+  REQUIRE(pq.peek() == 108);
   pq.remove();
+  REQUIRE(pq.peek() == 101);
   pq.remove();
-  assert(pq.isEmpty());
-
-  return 0;
+  REQUIRE(pq.peek() == 100);
+  pq.remove();
+  REQUIRE(pq.peek() == 78);
+  pq.remove();
+  REQUIRE(pq.peek() == 27);
+  pq.remove();
+  REQUIRE(pq.peek() == 25);
+  pq.remove();
+  REQUIRE(pq.peek() == 24);
+  pq.remove();
+  REQUIRE(pq.peek() == 10);
+  pq.remove();
+  REQUIRE(pq.peek() == 2);
+  pq.remove();
+  REQUIRE(pq.peek() == 1);
+  pq.remove();
+  REQUIRE(pq.isEmpty());
 }
